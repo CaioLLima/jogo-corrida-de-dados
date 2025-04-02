bf4f852f9304eac4a7526b6f3a440d37f239847e
CaioLLima <caiololima2@gmail.com>
2025-04-01 21:32:34 -0300

    Adicionado Avanço e recuo casas especiais.

--- JogoCorridaDeDados/Program.cs
+++ JogoCorridaDeDados/Program.cs
@@ -22,9 +22,40 @@
                     //Exibição do lançamento
                     ExibirLançamento(resultado);
 
-                    //Atualiza posicao usuario
+                    //Atualiza posicao usuario  
+                    //Caiu em uma casa normal
                     posicaoUsuario += resultado;
 
+                    //Avanço Extra
+                    if (posicaoUsuario == 5 || posicaoUsuario == 10 || posicaoUsuario == 15)
+                    {
+                        
+                        Console.WriteLine("Caiu em uma casa especial, avance 3 casas.");
+                        Console.WriteLine("----------------------------------");
+                        posicaoUsuario += 3;
+                    }
+                        
+                    //Recuo
+                    else if (posicaoUsuario == 7 || posicaoUsuario == 13 || posicaoUsuario == 20)
+                    {
+                        
+                        Console.WriteLine("Caiu em uma casa especial, retorne 2 casas.");
+                        Console.WriteLine("----------------------------------");
+                        posicaoUsuario -=  2;
+                    }
+                       
+
+                    // Rodada extra
+                    if (resultado == 6)
+
+                    {
+                        Console.WriteLine($"O jogador está na posição: {posicaoUsuario} de {LIMITELINHACHEGADA}.");
+                        Console.WriteLine("----------------------------------");
+                        Console.WriteLine("\nVocê tirou 6 no dado, pode jogar o dado novamente!!");
+                        continue;
+
+                    }
+                   
                     //Verifica se usuario ganhou
                     if (posicaoUsuario >= LIMITELINHACHEGADA)
                     {
@@ -46,10 +77,53 @@
                     //Exibição do lançamento
                     ExibirLançamento(resultadoComputador);
 
-                    //Atualiza posicao usuario
+                    //Atualiza posicao computador
                     posicaoComputador += resultadoComputador;
+                    //Avanço Extra
+                    if (posicaoComputador == 5 || posicaoComputador == 10 || posicaoComputador == 15)
+                    {
+                        
+                        Console.WriteLine("O computador caiu em uma casa especial, avançou 3 casas.");
+                        Console.WriteLine("----------------------------------");
+                        posicaoComputador += 3;
+                    }
+                        
+                    //Recuo
+                    else if (posicaoComputador == 7 || posicaoComputador == 13 || posicaoComputador == 20)
+                    {
+                        
+                        Console.WriteLine("O computador caiu em uma casa especial, retornou 2 casas.");
+                        Console.WriteLine("----------------------------------");
+                        posicaoComputador -= 2;
+                    }
+                        
 
-                    //Verifica se usuario ganhou
+                    // Rodada extra
+                    if (resultadoComputador == 6)
+                    {
+                        Console.WriteLine($"O Computador está na posição: {posicaoComputador} de {LIMITELINHACHEGADA}.");
+                        Console.WriteLine("----------------------------------");
+                        Console.WriteLine("\nO computador tirou 6 no dado, irá jogar o dado novamente!!");
+                        resultadoComputador = SorteioLançamentoDados();
+                        ExibirLançamento(resultadoComputador);
+                        posicaoComputador += resultadoComputador;
+                        if (posicaoComputador == 5 || posicaoComputador == 10 || posicaoComputador == 15)
+                        {
+                            Console.WriteLine("O computador caiu em uma casa especial, avançou 3 casas.");
+                            Console.WriteLine("----------------------------------");
+                            posicaoComputador += 3;
+                        }
+
+                        //Recuo
+                        else if (posicaoComputador == 7 || posicaoComputador == 13 || posicaoComputador == 20)
+                        {
+                            Console.WriteLine("O computador caiu em uma casa especial, retornou 2 casas.");
+                            Console.WriteLine("----------------------------------");
+                            posicaoComputador -= 2;
+                        }
+                    }
+
+                    //Verifica se computador ganhou
                     if (posicaoComputador >= LIMITELINHACHEGADA)
                     {
                         Console.WriteLine($"O computador chegou a linha de chegada.");
@@ -76,10 +150,16 @@
             Console.WriteLine("Jogo dos Dados");
             Console.WriteLine("----------------------------------");
             Console.WriteLine($"Turno do {nome}");
-            Console.WriteLine("----------------------------------");
+            
 
-            Console.Write("Pressione ENTER para lançar o dado...");
-            Console.ReadLine();
+            if(nome == "Usuario")
+            {
+                Console.WriteLine("----------------------------------");
+                Console.Write("Pressione ENTER para lançar o dado...");
+                Console.ReadLine();
+            }
+            
+                
         }
 
         static int SorteioLançamentoDados()
