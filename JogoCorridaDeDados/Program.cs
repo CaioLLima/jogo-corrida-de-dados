0eaea1c9e36613d3966c0461012b65a3c5b9b79d
CaioLLima <caiololima2@gmail.com>
2025-04-02 22:25:28 -0300

    Estruturando jogo

--- JogoCorridaDeDados/Program.cs
+++ JogoCorridaDeDados/Program.cs
@@ -27,45 +27,16 @@
                     posicaoUsuario += resultado;
 
                     //Avanço Extra
-                    if (posicaoUsuario == 5 || posicaoUsuario == 10 || posicaoUsuario == 15)
-                    {
-                        
-                        Console.WriteLine("Caiu em uma casa especial, avance 3 casas.");
-                        Console.WriteLine("----------------------------------");
-                        posicaoUsuario += 3;
-                    }
-                        
-                    //Recuo
-                    else if (posicaoUsuario == 7 || posicaoUsuario == 13 || posicaoUsuario == 20)
-                    {
-                        
-                        Console.WriteLine("Caiu em uma casa especial, retorne 2 casas.");
-                        Console.WriteLine("----------------------------------");
-                        posicaoUsuario -=  2;
-                    }
-                       
+                    posicaoUsuario = AvançoExtra(posicaoUsuario);
+
+                    //Recuo 
+                    posicaoUsuario = PenalidadeRecuar(posicaoUsuario);
 
                     // Rodada extra
-                    if (resultado == 6)
+                    posicaoUsuario = RodadaExtra("Usuário", resultado, posicaoUsuario, LIMITELINHACHEGADA);
 
-                    {
-                        Console.WriteLine($"O jogador está na posição: {posicaoUsuario} de {LIMITELINHACHEGADA}.");
-                        Console.WriteLine("----------------------------------");
-                        Console.WriteLine("\nVocê tirou 6 no dado, pode jogar o dado novamente!!");
-                        continue;
-
-                    }
-                   
                     //Verifica se usuario ganhou
-                    if (posicaoUsuario >= LIMITELINHACHEGADA)
-                    {
-                        Console.WriteLine($"O jogador chegou a linha de chegada.");
-                        jogoEmAndamento = false;
-                        Console.ReadLine();
-                    }
-                    else
-                        Console.WriteLine($"O jogador está na posição: {posicaoUsuario} de {LIMITELINHACHEGADA}.");
-                        Console.ReadLine();
+                    jogoEmAndamento = VerificaSeJogoTerminou("Usuário", posicaoUsuario, LIMITELINHACHEGADA, jogoEmAndamento);
 
                     //Computador
                     //Cabeçalho
@@ -80,63 +51,23 @@
                     //Atualiza posicao computador
                     posicaoComputador += resultadoComputador;
                     //Avanço Extra
-                    if (posicaoComputador == 5 || posicaoComputador == 10 || posicaoComputador == 15)
-                    {
-                        
-                        Console.WriteLine("O computador caiu em uma casa especial, avançou 3 casas.");
-                        Console.WriteLine("----------------------------------");
-                        posicaoComputador += 3;
-                    }
-                        
+                    posicaoComputador = AvançoExtra(posicaoComputador);
                     //Recuo
-                    else if (posicaoComputador == 7 || posicaoComputador == 13 || posicaoComputador == 20)
-                    {
-                        
-                        Console.WriteLine("O computador caiu em uma casa especial, retornou 2 casas.");
-                        Console.WriteLine("----------------------------------");
-                        posicaoComputador -= 2;
-                    }
-                        
+                    posicaoComputador = PenalidadeRecuar(posicaoComputador);
 
                     // Rodada extra
-                    if (resultadoComputador == 6)
-                    {
-                        Console.WriteLine($"O Computador está na posição: {posicaoComputador} de {LIMITELINHACHEGADA}.");
-                        Console.WriteLine("----------------------------------");
-                        Console.WriteLine("\nO computador tirou 6 no dado, irá jogar o dado novamente!!");
-                        resultadoComputador = SorteioLançamentoDados();
-                        ExibirLançamento(resultadoComputador);
-                        posicaoComputador += resultadoComputador;
-                        if (posicaoComputador == 5 || posicaoComputador == 10 || posicaoComputador == 15)
-                        {
-                            Console.WriteLine("O computador caiu em uma casa especial, avançou 3 casas.");
-                            Console.WriteLine("----------------------------------");
-                            posicaoComputador += 3;
-                        }
-
-                        //Recuo
-                        else if (posicaoComputador == 7 || posicaoComputador == 13 || posicaoComputador == 20)
-                        {
-                            Console.WriteLine("O computador caiu em uma casa especial, retornou 2 casas.");
-                            Console.WriteLine("----------------------------------");
-                            posicaoComputador -= 2;
-                        }
-                    }
+                    posicaoComputador = RodadaExtra("Computador", resultado, posicaoComputador, LIMITELINHACHEGADA);
 
                     //Verifica se computador ganhou
-                    if (posicaoComputador >= LIMITELINHACHEGADA)
-                    {
-                        Console.WriteLine($"O computador chegou a linha de chegada.");
-                        jogoEmAndamento = false;
-                        Console.ReadLine();
-                    }
-                    else
-                        Console.WriteLine($"O computador está na posição: {posicaoComputador} de {LIMITELINHACHEGADA}.");
-                        Console.ReadLine();
-                   
+                    jogoEmAndamento = VerificaSeJogoTerminou("Computador", posicaoComputador, LIMITELINHACHEGADA, jogoEmAndamento);
+
                 } while (jogoEmAndamento);
 
                 //Verificar se quer executar novamente
+                //while(true)
+                // IniciarJogo();
+                //if (!VerificaSeQuerNovamente)
+                //break;
                 string opcaoContinuar = PerguntaJogarNovamente();
                 if (opcaoContinuar != "s")
                     break;
@@ -175,6 +106,121 @@
             Console.WriteLine("----------------------------------");
             Console.WriteLine($"O valor sorteado foi {resultado}");
             Console.WriteLine("----------------------------------");
+        }
+
+        static int AvançoExtra(int posicao)
+        {
+            if (posicao == 5 || posicao == 10 || posicao == 15)
+            {
+                Console.WriteLine("Caiu em uma casa especial, avançará 3 casas.");
+                Console.WriteLine("----------------------------------");
+                return posicao += 3;
+            }
+            return posicao;
+        }
+
+        static int PenalidadeRecuar(int posicao)
+        {
+            if (posicao == 7 || posicao == 13 || posicao == 20)
+            {
+
+                Console.WriteLine("Caiu em uma casa especial, retornará 2 casas.");
+                Console.WriteLine("----------------------------------");
+                return posicao -= 2;
+            }
+            return posicao;
+        }
+
+        static int RodadaExtra(string nome, int resultado, int posicao, int LIMITELINHACHEGADA)
+        {
+            if (nome == "Usuário" && resultado == 6)
+            {
+                Console.WriteLine($"O jogador está na posição: {posicao} de {LIMITELINHACHEGADA}.");
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("\nVocê tirou 6 no dado, pode jogar o dado novamente!!");
+                //Cabeçalho
+                ExibirCabeçalho("Usuario");
+
+                //Sorteio do lançamento do dado
+                resultado = SorteioLançamentoDados();
+
+                //Exibição do lançamento
+                ExibirLançamento(resultado);
+
+                //Atualiza posicao usuario  
+                //Caiu em uma casa normal
+                posicao += resultado;
+
+                //Avanço Extra
+                posicao = AvançoExtra(posicao);
+
+                //Recuo 
+                posicao = PenalidadeRecuar(posicao);
+
+                return posicao;
+            }
+
+            else if(nome == "Computador" && resultado == 6)
+            {
+                Console.WriteLine($"O computador está na posição: {posicao} de {LIMITELINHACHEGADA}.");
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("\nO computador tirou 6 no dado, irá jogar o dado novamente!!");
+                //Cabeçalho
+                ExibirCabeçalho("Computador");
+
+                //Sorteio do lançamento do dado
+                resultado = SorteioLançamentoDados();
+
+                //Exibição do lançamento
+                ExibirLançamento(resultado);
+
+                //Atualiza posicao usuario  
+                //Caiu em uma casa normal
+                posicao += resultado;
+
+                //Avanço Extra
+                posicao = AvançoExtra(posicao);
+
+                //Recuo 
+                posicao = PenalidadeRecuar(posicao);
+
+                return posicao;
+            }
+            return posicao;
+
+        }
+
+        static bool VerificaSeJogoTerminou(string nome, int posicao, int LIMITELINHACHEGADA, bool jogoEmAndamento)
+        {
+            if (nome == "Usuário" && posicao >= LIMITELINHACHEGADA)
+            {
+                Console.WriteLine($"Parabéns, o jogador chegou a linha de chegada.");
+
+                Console.Write("\nPressione ENTER para continuar...");
+                Console.ReadLine();
+                return jogoEmAndamento = false;
+            }
+            else if(nome == "Usuário" && posicao < LIMITELINHACHEGADA) { 
+                Console.WriteLine($"O jogador está na posição: {posicao} de {LIMITELINHACHEGADA}.");
+                Console.Write("\nPressione ENTER para continuar...");
+                Console.ReadLine();
+                return jogoEmAndamento = true; 
+            }
+            else if (nome == "Computador" && posicao >= LIMITELINHACHEGADA)
+            {
+                Console.WriteLine($"O jogador chegou a linha de chegada.");
+                Console.Write("\nPressione ENTER para continuar...");
+                Console.ReadLine();
+                return jogoEmAndamento = false;
+            }
+            else if (nome == "Computador" && posicao < LIMITELINHACHEGADA)
+            { 
+                Console.WriteLine($"O jogador está na posição: {posicao} de {LIMITELINHACHEGADA}.");
+                Console.Write("\nPressione ENTER para continuar...");
+                Console.ReadLine();
+                return jogoEmAndamento = true; 
+            }
+            return jogoEmAndamento;
         }
 
         static string PerguntaJogarNovamente()
